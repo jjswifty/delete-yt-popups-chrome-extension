a99790340ef62748adaac4eb38fc812b9293392d
jjswift <supercheater2018@yandex.ru>
2023-01-22 21:23:56 +0300

    feat(utils): change await to promise all

--- src/shared/utils/send-message-from-popup.ts
+++ src/shared/utils/send-message-from-popup.ts
@@ -1,27 +1,11 @@
 import { BaseChromeMessage } from '../types/chrome-message-types';
 
 export const sendMessageFromPopupToAllTabs = async (msg: BaseChromeMessage) => {
-    // const tabs = await chrome.tabs.query({
-    //     currentWindow: true,
-    //     active: true
-    // });
-    //  все работает тут
-    // for (const tab of tabs) {
-    //     if (tab.id) {
-    //         await chrome.tabs.sendMessage(tab.id, msg);
-    //     }
-    // }
-
     const tabs = await chrome.tabs.query({
-        lastFocusedWindow: true,
-        active: true,
         url: 'https://*.youtube.com/*'
     });
 
-    for (const tab of tabs) {
-        if (tab.id) {
-            await chrome.tabs.sendMessage(tab.id, msg);
-        }
-    }
-
+    await Promise.all(tabs.map(async (tab) => {
+        tab.id && await chrome.tabs.sendMessage(tab.id, msg)
+    }))
 }
