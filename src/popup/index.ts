4e8ccc2c0dbea13dcf1e24bd184db499776adb6e
Edmon Narmetov <enarmetov@alfabank.ru>
2023-01-23 17:19:05 +0300

    style(*): prettier

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -7,34 +7,32 @@ import { ChromeMessage } from '../shared/types';
     const buttonStatus = await safelyGetFromSyncStorage('buttonStatus');
 
     if (buttonStatus) {
-        toggleButton.checked = buttonStatus.enabled
+        toggleButton.checked = buttonStatus.enabled;
 
         toggleButton.onclick = async () => {
             await chrome.runtime.sendMessage({
-                type: buttonStatus.enabled ?
-                    ChromeMessage.ENABLE_POPUP_SHOW :
-                    ChromeMessage.DISABLE_POPUP_SHOW
-            })
+                type: buttonStatus.enabled
+                    ? ChromeMessage.ENABLE_POPUP_SHOW
+                    : ChromeMessage.DISABLE_POPUP_SHOW,
+            });
             await setToSyncStorage({
-                buttonStatus
-            })
-        }
+                buttonStatus,
+            });
+        };
     } else {
-        toggleButton.checked = false
+        toggleButton.checked = false;
 
         toggleButton.onclick = async () => {
             await chrome.runtime.sendMessage({
-                type: ChromeMessage.DISABLE_POPUP_SHOW
-            })
+                type: ChromeMessage.DISABLE_POPUP_SHOW,
+            });
             await setToSyncStorage({
                 buttonStatus: {
-                    enabled: false
-                }
-            })
-        }
+                    enabled: false,
+                },
+            });
+        };
     }
-
-
 })();
 
 export {};
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -2,8 +2,4 @@ import { waitForElementToRender } from './wait-for-element-to-render';
 import { safelyGetFromSyncStorage } from './safely-get-from-storage';
 import { setToSyncStorage } from './set-to-storage';
 
-export {
-    waitForElementToRender,
-    safelyGetFromSyncStorage,
-    setToSyncStorage,
-};
+export { waitForElementToRender, safelyGetFromSyncStorage, setToSyncStorage };
