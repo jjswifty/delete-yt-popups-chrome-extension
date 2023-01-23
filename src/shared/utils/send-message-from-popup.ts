52e80a2dd6ec4c5820fe57de3339a68c6d469aad
Edmon Narmetov <enarmetov@alfabank.ru>
2023-01-23 17:28:39 +0300

    style(prettier): fix

--- src/shared/utils/send-message-from-popup.ts
+++ src/shared/utils/send-message-from-popup.ts
@@ -2,10 +2,12 @@ import { BaseChromeMessage } from '../types';
 
 export const sendMessageFromPopupToAllTabs = async (msg: BaseChromeMessage) => {
     const tabs = await chrome.tabs.query({
-        url: 'https://*.youtube.com/*'
+        url: 'https://*.youtube.com/*',
     });
 
-    await Promise.all(tabs.map(async (tab) => {
-        tab.id && await chrome.tabs.sendMessage(tab.id, msg)
-    }))
-}
+    await Promise.all(
+        tabs.map(async (tab) => {
+            tab.id && (await chrome.tabs.sendMessage(tab.id, msg));
+        }),
+    );
+};
